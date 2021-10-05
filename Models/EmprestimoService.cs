d2d55ebc124fa752a443346c08d93dcc0f12aff1
rog85 <rogeriotrusz@live.com>
2021-10-05 19:12:25 -0300

    Listagem de Emprestimo exibe por ordem decrescente da data de devolucao

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -2,47 +2,37 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
-namespace Biblioteca.Models
-{
-    public class EmprestimoService 
-    {
-        public void Inserir(Emprestimo e)
-        {
-            using(BibliotecaContext bc = new BibliotecaContext())
-            {
-                bc.Emprestimos.Add(e);
-                bc.SaveChanges();
+namespace Biblioteca.Models {
+    public class EmprestimoService {
+        public void Inserir (Emprestimo e) {
+            using (BibliotecaContext bc = new BibliotecaContext ()) {
+                bc.Emprestimos.Add (e);
+                bc.SaveChanges ();
             }
         }
 
-        public void Atualizar(Emprestimo e)
-        {
-            using(BibliotecaContext bc = new BibliotecaContext())
-            {
-                Emprestimo emprestimo = bc.Emprestimos.Find(e.Id);
+        public void Atualizar (Emprestimo e) {
+            using (BibliotecaContext bc = new BibliotecaContext ()) {
+                Emprestimo emprestimo = bc.Emprestimos.Find (e.Id);
                 emprestimo.NomeUsuario = e.NomeUsuario;
                 emprestimo.Telefone = e.Telefone;
                 emprestimo.LivroId = e.LivroId;
                 emprestimo.DataEmprestimo = e.DataEmprestimo;
                 emprestimo.DataDevolucao = e.DataDevolucao;
 
-                bc.SaveChanges();
+                bc.SaveChanges ();
             }
         }
 
-        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
-        {
-            using(BibliotecaContext bc = new BibliotecaContext())
-            {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+        public ICollection<Emprestimo> ListarTodos (FiltrosEmprestimos filtro) {
+            using (BibliotecaContext bc = new BibliotecaContext ()) {
+                return bc.Emprestimos.Include (e => e.Livro).OrderByDescending (e => e.DataDevolucao).ToList ();
             }
         }
 
-        public Emprestimo ObterPorId(int id)
-        {
-            using(BibliotecaContext bc = new BibliotecaContext())
-            {
-                return bc.Emprestimos.Find(id);
+        public Emprestimo ObterPorId (int id) {
+            using (BibliotecaContext bc = new BibliotecaContext ()) {
+                return bc.Emprestimos.Find (id);
             }
         }
     }
