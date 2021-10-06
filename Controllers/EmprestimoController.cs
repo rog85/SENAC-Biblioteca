0de52a9c2ef01aceaf96d47444e2ea23d19368b4
rog85 <rogeriotrusz@live.com>
2021-10-05 21:35:32 -0300

    Adicionado autenticacao nas paginas de emprestimo

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -1,64 +1,59 @@
+using System;
 using Biblioteca.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-using System;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Biblioteca.Controllers
-{
-    
-    public class EmprestimoController : Controller
-    {
-        public IActionResult Cadastro()
-        {
-            LivroService livroService = new LivroService();
-            EmprestimoService emprestimoService = new EmprestimoService();
+namespace Biblioteca.Controllers {
 
-            CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarDisponiveis();
-            return View(cadModel);
+    public class EmprestimoController : Controller {
+
+        public IActionResult Cadastro () {
+            Autenticacao.CheckLogin (this);
+
+            LivroService livroService = new LivroService ();
+            EmprestimoService emprestimoService = new EmprestimoService ();
+
+            CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel ();
+            cadModel.Livros = livroService.ListarDisponiveis ();
+            return View (cadModel);
         }
 
         [HttpPost]
-        public IActionResult Cadastro(CadEmprestimoViewModel viewModel)
-        {
-            EmprestimoService emprestimoService = new EmprestimoService();
-            
-            if(viewModel.Emprestimo.Id == 0)
-            {
-                emprestimoService.Inserir(viewModel.Emprestimo);
+        public IActionResult Cadastro (CadEmprestimoViewModel viewModel) {
+            EmprestimoService emprestimoService = new EmprestimoService ();
+
+            if (viewModel.Emprestimo.Id == 0) {
+                emprestimoService.Inserir (viewModel.Emprestimo);
+            } else {
+                emprestimoService.Atualizar (viewModel.Emprestimo);
             }
-            else
-            {
-                emprestimoService.Atualizar(viewModel.Emprestimo);
-            }
-            return RedirectToAction("Listagem");
+            return RedirectToAction ("Listagem");
         }
 
-        public IActionResult Listagem(string tipoFiltro, string filtro)
-        {
+        public IActionResult Listagem (string tipoFiltro, string filtro) {
+            Autenticacao.CheckLogin (this);
             FiltrosEmprestimos objFiltro = null;
-            if(!string.IsNullOrEmpty(filtro))
-            {
-                objFiltro = new FiltrosEmprestimos();
+            if (!string.IsNullOrEmpty (filtro)) {
+                objFiltro = new FiltrosEmprestimos ();
                 objFiltro.Filtro = filtro;
                 objFiltro.TipoFiltro = tipoFiltro;
             }
-            EmprestimoService emprestimoService = new EmprestimoService();
-            return View(emprestimoService.ListarTodos(objFiltro));
+            EmprestimoService emprestimoService = new EmprestimoService ();
+            return View (emprestimoService.ListarTodos (objFiltro));
         }
 
-        public IActionResult Edicao(int id)
-        {
-            LivroService livroService = new LivroService();
-            EmprestimoService em = new EmprestimoService();
-            Emprestimo e = em.ObterPorId(id);
+        public IActionResult Edicao (int id) {
+            Autenticacao.CheckLogin (this);
+            LivroService livroService = new LivroService ();
+            EmprestimoService em = new EmprestimoService ();
+            Emprestimo e = em.ObterPorId (id);
 
-            CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel();
-            cadModel.Livros = livroService.ListarTodos();
+            CadEmprestimoViewModel cadModel = new CadEmprestimoViewModel ();
+            cadModel.Livros = livroService.ListarTodos ();
             cadModel.Emprestimo = e;
-            
-            return View(cadModel);
+
+            return View (cadModel);
         }
     }
 }
